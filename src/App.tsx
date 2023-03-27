1816922dd0f0b0a5b71ba9f74697145d35315642
JunSeong_Park <ttotto0608@naver.com>
2023-03-28 02:21:49 +0900

    add webcam streaming and recording and download function

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {CAMERA_STATUS, RecordWebcamHook, RecordWebcamOptions, useRecordWebcam} from "react-record-webcam";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const OPTIONS: RecordWebcamOptions = {
+    filename: "test-filename",
+    fileType: "mp4",
+    width: 640,
+    height: 480
 }
 
-export default App;
+export const App = () => {
+
+    const recordWebcam: RecordWebcamHook = useRecordWebcam(OPTIONS);
+    const getRecordingFileHooks = async () => {
+        const blob = await recordWebcam.getRecording();
+        console.log(blob);
+    }
+
+    return (
+        <div className={"demo-section"}>
+            <h1>Webcam Recorder</h1>
+            <p>Camera Status : {recordWebcam.status}</p>
+            <div>
+                <button onClick={recordWebcam.open} disabled={
+                    recordWebcam.status === CAMERA_STATUS.OPEN ||
+                    recordWebcam.status === CAMERA_STATUS.RECORDING ||
+                    recordWebcam.status === CAMERA_STATUS.PREVIEW
+                }>
+                    Open Camera
+                </button>
+                <button onClick={recordWebcam.close} disabled={
+                    recordWebcam.status === CAMERA_STATUS.CLOSED ||
+                    recordWebcam.status === CAMERA_STATUS.PREVIEW
+                }>
+                    Close Camera
+                </button>
+                <button onClick={recordWebcam.start} disabled={
+                    recordWebcam.status === CAMERA_STATUS.CLOSED ||
+                    recordWebcam.status === CAMERA_STATUS.RECORDING ||
+                    recordWebcam.status === CAMERA_STATUS.PREVIEW
+                }>
+                    Start Recording
+                </button>
+                <button onClick={recordWebcam.stop} disabled={
+                    recordWebcam.status !== CAMERA_STATUS.RECORDING
+                }>
+                    Stop Recording
+                </button>
+                <button onClick={recordWebcam.retake} disabled={
+                    recordWebcam.status !== CAMERA_STATUS.PREVIEW
+                }>
+                    Retake
+                </button>
+                <button onClick={recordWebcam.download} disabled={
+                    recordWebcam.status !== CAMERA_STATUS.PREVIEW
+                }>
+                    Download Recording
+                </button>
+                <button onClick={getRecordingFileHooks} disabled={
+                    recordWebcam.status !== CAMERA_STATUS.PREVIEW
+                }>
+                    Get Recording
+                </button>
+            </div>
+
+            <video ref={recordWebcam.webcamRef} autoPlay src={""}
+            style={{
+                display: `${recordWebcam.status === CAMERA_STATUS.OPEN || 
+                    recordWebcam.status === CAMERA_STATUS.RECORDING
+                    ? "block"
+                    : "none"
+                }`
+            }}></video>
+
+            <video autoPlay ref={recordWebcam.previewRef}
+            style = {{
+                display: `${recordWebcam.status === CAMERA_STATUS.PREVIEW 
+                    ? "block"
+                    : "none"
+                }`
+            }}></video>
+        </div>
+    )
+}
